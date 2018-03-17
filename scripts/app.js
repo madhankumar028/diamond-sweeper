8ec743bf3e385a66d99791a4615f20f93192674f
madhankumar028 <madhankumar028@gmail.com>
2018-03-17 12:33:41 +0530

    UI background changes and loads the bg-image to all the boxes and adds an event to all boxes for flip event

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,34 @@
  */
 (function() {
     'use strict';
+    
+    const gridContainer = document.getElementsByClassName('game-container')[0];
 
-    console.log('App loads with basic layout');
+    const backgroundImage = {
+        'background-image': 'assets/images/question.png',
+        'background-position': 'center',
+        'background-repeat': 'no-repeat',
+        'background-size': 'contain'
+    };
+
+    function init() {
+        console.log('App loads with basic layout');
+
+        for (let gridItem of gridContainer.children) {
+            // adds Eventlistener for flipping the image
+            gridItem.addEventListener('click', flipImage);
+            
+            // adds background image to all the boxed
+            gridItem.style.backgroundImage = `url(${backgroundImage['background-image']})`;
+            gridItem.style.backgroundPosition = `${backgroundImage['background-position']}`;
+            gridItem.style.backgroundSize = `${backgroundImage['background-size']}`;
+            gridItem.style.backgroundRepeat = `${backgroundImage['background-repeat']}`;            
+        }
+    }
+
+    function flipImage() {
+        console.log('clicked for flip the image');
+    }
+
+    init();
 })();
