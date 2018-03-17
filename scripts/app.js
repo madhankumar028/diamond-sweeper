69dbc2d9d9f30b54362902e2adc0ca8fdf3825c8
madhankumar028 <madhankumar028@gmail.com>
2018-03-17 13:20:06 +0530

    adds multiple background images to all the boxes and boilerplate calculation for score and highest score

--- scripts/app.js
+++ scripts/app.js
@@ -9,10 +9,12 @@
     const gridContainer = document.getElementsByClassName('game-container')[0];
 
     const backgroundImage = {
-        'background-image': 'assets/images/question.png',
-        'background-position': 'center',
-        'background-repeat': 'no-repeat',
-        'background-size': 'contain'
+        'background-question-image': 'assets/images/question.png',
+        'background-diamond-image': 'assets/images/diamond.png',        
+        'background-question-position': '600px',
+        'background-diamond-position': '5px',        
+        'background-size': 'contain',
+        'background-repeat': 'no-repeat'
     };
 
     function init() {
@@ -22,16 +24,28 @@
             // adds Eventlistener for flipping the image
             gridItem.addEventListener('click', flipImage);
             
-            // adds background image to all the boxed
-            gridItem.style.backgroundImage = `url(${backgroundImage['background-image']})`;
-            gridItem.style.backgroundPosition = `${backgroundImage['background-position']}`;
-            gridItem.style.backgroundSize = `${backgroundImage['background-size']}`;
-            gridItem.style.backgroundRepeat = `${backgroundImage['background-repeat']}`;            
+            // adds multiple background image to all the boxes with both question and diamond
+            gridItem.style.backgroundImage      = `url(${backgroundImage['background-question-image']}), url(${backgroundImage['background-diamond-image']})`;
+            gridItem.style.backgroundPosition   = `${backgroundImage['background-question-position']}, ${backgroundImage['background-diamond-position']}`;
+            gridItem.style.backgroundSize       = `${backgroundImage['background-size']}, ${backgroundImage['background-size']}`;
+            gridItem.style.backgroundRepeat     = `${backgroundImage['background-repeat']}`;
         }
     }
 
-    function flipImage() {
+    function flipImage(event) {
+        console.log(event.target.getAttribute('class'));
         console.log('clicked for flip the image');
+    }
+
+    function calculateScore(remainingGrids) {
+        localStorage.setItem('HighestScore', remainingGrids);
+        
+        let score = remainingGrids;
+        let highestScore = localStorage.getItem('HighestScore');
+
+        score > highestScore
+            ? (localStorage.setItem('HighestScore', score), highestScore = score)
+            : '';
     }
 
     init();
